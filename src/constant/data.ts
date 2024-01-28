99194aecd0715785ad945c7554922fe28765f81d
Suresh Chaudhary <hello@suresh.im>
2024-01-28 10:57:29 +0530

    :art: renamed `info.ts` to `data.ts`

---
+++ src/constant/data.ts
@@ -0,0 +1,115 @@
+export const externals = {
+  javascript: "https://en.wikipedia.org/wiki/JavaScript",
+  compony: {
+    name: "Maxim AI",
+    site: "https://getmaxim.ai",
+  },
+  base_url: "https://suresh.im",
+  logo_text: "suresh.",
+  footer_logo_text: "Suresh Chaudhary",
+  name: "suresh",
+  meta_description:
+    "Full-stack engineer Suresh Chaudhary | Crafting digital excellence with code.",
+};
+
+export const projects = [
+  {
+    name: "vani.club",
+    description:
+      "YouTube Team Collaboration, Direct Uploads, and AI Magic for Effortless Content Creation!",
+    link: "https://vani.club",
+  },
+  {
+    name: "tini.fyi",
+    description:
+      "Generate Short Links Effortlessly for Simplified, Secure, and Ad-Free Sharing!",
+    link: "https://tini.fyi/from-portfolio",
+  },
+  {
+    name: "suresh.im",
+    description: "Personal site - Showcase Your Work with Simplicity!",
+    link: "https://github.com/impoiler/suresh.im",
+  },
+  {
+    name: "section.fyi",
+    description:
+      "Build Free Custom Page with Multiple Links for Effortless Online Presence!",
+    link: "https://section.fyi",
+  },
+  {
+    name: "tools",
+    description:
+      "Your All-in-One Toolbox with Image to Base64, Domain Tools, IP Checker, and More!",
+    link: "https://tools.suresh.im",
+  },
+  {
+    name: "gituser",
+    description:
+      "Discover GitHub User and Organization Details with a Sleek Interface",
+    link: "https://git.suresh.im",
+  },
+];
+
+export const Links = {
+  email: "mailto:hello@suresh.im",
+  github: "https://github.com/impoiler",
+  linkedin: "https://linkedin.com/in/impoiler",
+  peerlist: "https://peerlist.io/impoiler",
+  x: "https://twitter.com/elision_ims",
+  cal: "https://cal.com/impoiler",
+};
+
+export const experiences = [
+  {
+    companyName: "Maxim AI",
+    location: "Pune, India",
+    positions: [
+      {
+        name: "Founding FullStack Developer",
+        fromToTill: "Jan 2024",
+        type: "Full-Time",
+        notes: [
+          "Driven by a personal passion for AI and LLMs in general, I joined Maxim AI as a founding team member. In this role, I actively contributed to the establishment of a framework designed for testing and monitoring AI applications.",
+        ],
+      },
+    ],
+  },
+  {
+    companyName: "SocialWell",
+    location: "Remote, India",
+    positions: [
+      {
+        name: "FullStack Engineer",
+        fromToTill: "Jul 2023 - Jan 2024",
+        type: "Full-Time",
+        notes: [
+          "While serving as a Frontend Engineer at Socialwel, I proactively enhanced my skills, pushing myself to delve into full-stack engineering. My keen interest led me to explore the intricacies of the entire system, gaining insights into its end-to-end functionality.",
+          "To achieve this objective, I immersed myself in learning backend technologies like APIs, GraphQL, Protocols, Databases, and Status. Following a dedicated period of learning and practical application, I was honored with a promotion to the position of a full-stack engineer.",
+        ],
+      },
+      {
+        name: "Frontend Developer",
+        fromToTill: "Oct 2022 - Jan 2024",
+        type: "Full-Time",
+        notes: [
+          "At Socialwell, I refined my collaborative skills within dynamic team environments and embraced the intricacies of remote work culture. This experience not only granted me the ability to seamlessly work from my home but also introduced a welcomed flexibility into my professional routine.",
+          "As the entire team operated remotely, I proactively acquired essential skills and knowledge to align with this collaborative approach. This involved gaining proficiency in tools such as Git/GitHub for version control and embracing project management methodologies like sprints and scrums.",
+        ],
+      },
+    ],
+  },
+  {
+    companyName: "iSquare Technologies",
+    location: "Ahmedabad, India",
+    positions: [
+      {
+        name: "Frontend Developer",
+        fromToTill: "Mar 2022 - Oct 2022",
+        type: "Full-Time",
+        notes: [
+          "I started my career here and took my first step into the software engineering world. During the same time period, alongside my full-time job, I was upscaling myself from basic technologies like HTML, CSS, and JavaScript to more advanced techs like React, NextJS, NodeJS, etc.",
+        ],
+      },
+    ],
+  },
+];
