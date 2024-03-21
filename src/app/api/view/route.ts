58d586ce54c62d13a32b9feaba08c8ac0f4f6226
Suresh Chaudhary <hello@suresh.im>
2024-03-21 21:42:08 +0530

    :sparkles: page-view cookies to prevent repeat count

--- src/app/api/view/route.ts
+++ src/app/api/view/route.ts
@@ -1,11 +1,24 @@
 import { redis } from "@/lib/redis";
 import { NextResponse } from "next/server";
+import { cookies } from "next/headers";
 
 export async function POST(req: Request) {
   try {
     const { slug } = await req.json();
+    const cookie = cookies();
+    if (cookie.get(slug)?.value === "visited") {
+      const count = await redis.get(slug);
+      return NextResponse.json({ fine: true, count });
+    }
     const count = await redis.incr(slug);
-    return NextResponse.json({ count, fine: true });
+    return NextResponse.json(
+      { count, fine: true },
+      {
+        headers: {
+          "Set-Cookie": `${slug}=visited; Path=${slug} HttpOnly`,
+        },
+      }
+    );
   } catch (ignore) {
     return NextResponse.json({
       fine: false,
